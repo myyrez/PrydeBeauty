ff3be033fc796f963be02614de949f34ed3d9460
mathêe <matheusfc2626@gmail.com>
2023-10-06 06:28:53 -0300

    🚨 wishlist page 100% responsiva :o 🚨

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -24,9 +24,9 @@ export default async function WishlistPage() {
     return (
         <div className="relative flex justify-center h-fit text-stone-900">
             <Navbar />
-            <div className='absolute animate-spin-slow left-44 inset-y-0 mx-auto my-auto mt-20 h-56 w-40 rounded-full z-0 bg-gradient-to-b from-orange-400 to-pink-600 blur-3xl'></div>
+            <div className='absolute animate-spin-slow left-44 md:left-24 sm:left-24 sm:h-28 sm:w-28 inset-y-0 mx-auto my-auto mt-20 sm:mt-10 h-56 w-40 rounded-full z-0 bg-gradient-to-b from-orange-400 to-pink-600 blur-3xl'></div>
 
-            <div className="flex z-20 flex-col w-4/5 h-fit gap-6 p-4 mt-24">
+            <div className="flex z-20 flex-col w-4/5 md:w-full h-fit gap-6 p-4 mt-24 sm:mt-12 sm:pt-0 sm:mb-24">
                 <h1 className="text-4xl">Your <span className='italic font-serif'>wishlist</span></h1>
 
                 {wishlists.map(wishlist => (
--- components/WishlistCard/index.tsx
+++ components/WishlistCard/index.tsx
@@ -61,37 +61,39 @@ const WishlistCard: React.FunctionComponent<WishlistCardProps> = async (props) =
     let discountedCents = (productPriceSum - ((productPriceSum * discountPercentage) / 100)).toFixed(2)
 
     return (
-        <div className="relative flex w-full items-start gap-2 my-1 h-[200px] bg-stone-400/5 backdrop-blur-sm">
-            <WishlistButton setWishlisted={setWishlisted} {...{ id, isProductWishlisted }} />
+        <div className="relative flex w-full items-start gap-2 my-1 h-[200px] sm:h-[150px] sm:mb-12 bg-stone-400/5 backdrop-blur-sm">
+            <div className='sm:absolute right-0 bottom-5'>
+                <WishlistButton setWishlisted={setWishlisted} {...{ id, isProductWishlisted }} />
+            </div>
 
-            <Image src={`/images/${id}.jpg`} height={200} width={150} alt='product card image' />
+            <Image src={`/images/${id}.jpg`} className='sm:h-[150px] sm:w-[110px]' height={200} width={150} alt='product card image' />
             <div className="flex flex-col justify-between w-fit p-2 gap-2">
                 <Link href={`/product/${id}`} className='w-fit'>
-                    <h1 className="text-xl font-bold">
+                    <h1 className="text-xl md:text-base font-bold">
                         {name}
                         <div className='h-px w-1/6 ease-in-out duration-300 bg-zinc-900'></div>
                     </h1>
                 </Link>
-                <h1 className="text-xl font-bold">{collection} <span className='italic font-serif'>Collection</span></h1>
-                <div className="flex gap-2  mt-2 items-center">
+                <h1 className="text-xl md:text-base font-bold">{collection} <span className='italic font-serif sm:hidden'>Collection</span></h1>
+                <div className="flex gap-2 mt-2 items-center sm:hidden">
                     <p>Overall reviews:</p>
                     <div className='flex gap-1 items-center font-semibold'> <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /> </div>
                 </div>
-                <h1 className="font-semibold text-lg text-green-700">In stock</h1>
+                <h1 className="font-semibold text-lg text-green-700 sm:hidden">In stock</h1>
             </div>
 
-            <div className='flex absolute right-4 bottom-8 justify-center w-36 h-11 mx-auto border-solid border-2 border-zinc-900'>
+            <div className='flex absolute right-4 bottom-8 sm:bottom-0 sm:right-20 sm:translate-y-full justify-center w-36 h-11 mx-auto border-solid border-2 border-zinc-900'>
                 {/* <p className='h-auto m-auto text-sm leading-[30px] font-bold'>Add to Cart</p> */}
                 <AddToCartButton {...{pageProductId: id, isProductCarted}} />
             </div>
             {discountPercentage > 0 
-                ?   <div className='flex absolute right-40 bottom-8 justify-center w-24 h-11 mx-auto py-1 px-4 border-solid border-2 bg-zinc-50 text-zinc-900 border-zinc-900'>
+                ?   <div className='flex absolute right-40 bottom-8 sm:hidden justify-center w-24 h-11 mx-auto py-1 px-4 border-solid border-2 bg-zinc-50 text-zinc-900 border-zinc-900'>
                         <h1 className="flex h-auto m-auto gap-1 text-xs font-bold">
                             $ <span className="text-xl leading-none">{discountedPrice}</span> {discountedCents.split('.')[1]}
                         </h1>
                     </div>
 
-                :   <div className='flex absolute right-40 bottom-8 justify-center w-24 h-11 mx-auto py-1 px-4 border-solid border-2 bg-zinc-50 text-zinc-900 border-zinc-900'>
+                :   <div className='flex absolute right-40 bottom-8 sm:hidden justify-center w-24 h-11 mx-auto py-1 px-4 border-solid border-2 bg-zinc-50 text-zinc-900 border-zinc-900'>
                         <h1 className="flex h-auto m-auto gap-1 text-xs font-bold">
                             $ <span className="text-xl leading-none">{listPrice}</span> {listPriceCents}
                         </h1>
